1481f68404fdfdd6e82e54047ea329d2e401ecc5
Tigran20 <Podliza122@bk.ru>
2018-02-03 14:01:25 +0300

    phone

--- HelloAndroid/app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ HelloAndroid/app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.android.helloandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
     }
 }
